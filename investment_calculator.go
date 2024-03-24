382a068d8c99c069a6aabde9b2ff3db317ca44d3
dani_edo <dani.edogawa.17@gmail.com>
2024-03-24 14:10:40 +0700

    feat: add input field to assign value

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,9 +8,20 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	var investmentAmount, years float64
+	expectedReturnRate := 5.5
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
-	fmt.Println(futureValue, futureRealValue)
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
