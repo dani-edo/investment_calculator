de8a65dab7b9951caef25c4597cc043970909ed8
dani_edo <dani.edogawa.17@gmail.com>
2024-03-24 13:10:33 +0700

    feat: add inflationRate

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,8 +7,10 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-	fmt.Println(futureValue)
+	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
+	fmt.Println(futureValue, futureRealValue)
 }
